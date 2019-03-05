89bc424fb1eb835ddbbb65b2abfac4f35c222ac8
rajeev <shopfry@gmail.com>
2019-03-05 21:03:01 +0530

    Adding new images

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './icon.png';
 import banner from './logo.png';
 import saiBaba from './saiBaba.jpg';
+import vignesha from './lord1.png';
 import './style.css';
 
 class Home extends Component {
@@ -24,27 +25,31 @@ class Home extends Component {
   render() {
     return (
       <div className="App w3-center">
-        <header className="App-header">
-          <h1 className="App-title">&nbsp;<img src={logo} className="App-logo" alt="logo" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src={banner} alt="Sai Baba" /></h1>
-        </header>
-        <p className="App-intro">
+        <p>
+          <h1 className="App-title">&nbsp;<img src={logo} className="App-logo" alt="logo" /></h1>
+        </p>
+        <p className="w3-row w3-center">
           <img src={saiBaba} className="w3-image" alt="Sai Baba" />
+          <input className="w3-input w3-black w3-center w3-padding-32" type="text" placeholder="Pray, what you wish" required name="Pray" onKeyPress={this.keyPress} />
         </p>
         <div className="prayText">{this.state.prayText}</div>
+        <p className="w3-row w3-center">
+          <img src={vignesha} className="w3-image" alt="Om Ganapathi Bapa" />
+        </p>
         <div className="w3-container">
-          <p><input className="w3-input w3-padding-16" type="text" placeholder="Pray, what you wish" required name="Pray" onKeyPress={this.keyPress} /></p>
           <div className="w3-card-4 w3-cell-row w3-center ">
-              <div className="w3-container w3-white w3-cell w3-hover-orange">
+              <div className="w3-container w3-sand w3-cell w3-hover-orange">
                 <a onClick={false/*this.gotoNeedHelp*/}>{/*I Need<br />Help*/}</a>
                 <p>{/*I Can <br />Help*/}OM</p>
               </div>
               <div className="w3-container w3-orange w3-cell w3-hover-orange">
                 <p>{/*I Can <br />Help*/}SAI</p>
               </div>
-              <div className="w3-container w3-white w3-cell w3-hover-orange">
+              <div className="w3-container w3-sand w3-cell w3-hover-orange">
                 <p>{/*Help for<br/>someone*/}RAM</p>
               </div>
           </div>
+          <br/><br/>
         </div>
       </div>
     );
